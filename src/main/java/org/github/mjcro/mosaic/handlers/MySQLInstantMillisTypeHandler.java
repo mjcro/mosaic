d160c5e5c6e13dc01b6e7c4554fca4c4a6580798
gotterdemarung <gotterdemarung@gmail.com>
2023-03-21 14:32:52 +0200

    :sparkles: Instant type handlers

---
+++ src/main/java/org/github/mjcro/mosaic/handlers/MySQLInstantMillisTypeHandler.java
@@ -0,0 +1,26 @@
+package org.github.mjcro.mosaic.handlers;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+
+public class MySQLInstantMillisTypeHandler extends MySQLAbstractTypeHandler {
+    public MySQLInstantMillisTypeHandler() {
+        super("Instant", "`value`");
+    }
+
+    @Override
+    protected void setPlaceholdersValue(PreparedStatement stmt, int offset, Object value) throws SQLException {
+        if (value instanceof Instant) {
+            stmt.setLong(offset, ((Instant) value).toEpochMilli());
+        } else {
+            throw new UnexpectedValueException(value);
+        }
+    }
+
+    @Override
+    protected Object readObjectValue(ResultSet resultSet, int offset) throws SQLException {
+        return Instant.ofEpochMilli(resultSet.getLong(offset));
+    }
+}
---
+++ src/main/java/org/github/mjcro/mosaic/handlers/MySQLInstantSecondsTypeHandler.java
@@ -0,0 +1,26 @@
+package org.github.mjcro.mosaic.handlers;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+
+public class MySQLInstantSecondsTypeHandler extends MySQLAbstractTypeHandler {
+    public MySQLInstantSecondsTypeHandler() {
+        super("Instant", "`value`");
+    }
+
+    @Override
+    protected void setPlaceholdersValue(PreparedStatement stmt, int offset, Object value) throws SQLException {
+        if (value instanceof Instant) {
+            stmt.setLong(offset, ((Instant) value).getEpochSecond());
+        } else {
+            throw new UnexpectedValueException(value);
+        }
+    }
+
+    @Override
+    protected Object readObjectValue(ResultSet resultSet, int offset) throws SQLException {
+        return Instant.ofEpochSecond(resultSet.getLong(offset));
+    }
+}
