b1f87b234a57b6f94c12226b49736f9c93f95e6e
gotterdemarung <gotterdemarung@gmail.com>
2023-03-21 12:05:19 +0200

    :sparkles: Long type handler

---
+++ src/main/java/org/github/mjcro/mosaic/handlers/MySQLLongTypeHandler.java
@@ -0,0 +1,25 @@
+package org.github.mjcro.mosaic.handlers;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MySQLLongTypeHandler extends MySQLAbstractTypeHandler {
+    public MySQLLongTypeHandler() {
+        super("Long", "`value`");
+    }
+
+    @Override
+    protected void setPlaceholdersValue(PreparedStatement stmt, int offset, Object value) throws SQLException {
+        if (value instanceof Long) {
+            stmt.setLong(offset, (Long) value);
+        } else {
+            throw new UnexpectedValueException(value);
+        }
+    }
+
+    @Override
+    protected Object readObjectValue(ResultSet resultSet, int offset) throws SQLException {
+        return resultSet.getLong(offset);
+    }
+}
