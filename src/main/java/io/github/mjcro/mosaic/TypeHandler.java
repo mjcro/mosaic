deaeae433ae5db4efbac41003cdcf39efe59d2f7
gotterdemarung <gotterdemarung@gmail.com>
2023-03-23 11:46:28 +0200

    :fire: Remove redundant utility

--- src/main/java/io/github/mjcro/mosaic/TypeHandler.java
+++ src/main/java/io/github/mjcro/mosaic/TypeHandler.java
@@ -3,7 +3,6 @@ package io.github.mjcro.mosaic;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -27,31 +26,6 @@ public interface TypeHandler {
             Collection<Long> linkIds,
             Collection<Key> keys
     ) throws SQLException;
-
-    /**
-     * Fetches data from database.
-     *
-     * @param connection  Database connection to use.
-     * @param tablePrefix Database table prefix.
-     * @param linkId      Anchor link identifier.
-     * @param keys        Keys to read.
-     * @return Matched records.
-     * @throws SQLException On database error.
-     */
-    default <Key extends KeySpec> Map<Key, List<Object>> findByLinkId(
-            Connection connection,
-            String tablePrefix,
-            long linkId,
-            Collection<Key> keys
-    ) throws SQLException {
-        Map<Long, Map<Key, List<Object>>> data = findByLinkId(
-                connection, tablePrefix, Collections.singleton(linkId), keys
-        );
-        if (data.isEmpty()) {
-            return Collections.emptyMap();
-        }
-        return data.get(linkId);
-    }
 
     /**
      * Stores data.
